e294bcf5ccd39988a8d2baa000b4a5514a746a86
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-26 18:00:51 +0100

    add pry gems

--- spec/archivist_spec.rb
+++ spec/archivist_spec.rb
@@ -1,9 +1,57 @@
-RSpec.describe Archivist do
-  it "has a version number" do
-    expect(Archivist::VERSION).not_to be nil
+require 'spec_helper'
+
+describe "Archivist" do
+
+  path = "fixtures/project-folder"
+  let(:archivist) { Archivist::Archivist.new }
+  let (:empty_project_folder) {
+    file_name = File.basename(file_path)
+    dir = File.dirname(file_path)
+
+    Dir.foreach(dir) do |f|
+      if f != file_name or f != '.' or f != '..' 
+        if File.directory?(f) 
+          FileUtils.rm_rf(f)
+        else FileUtils.rm(f)
+      end
+    end
+  }
+
+  describe "#init" do
+    before do
+      empty_project_folder
+      archivist.init
+    end
+
+    it "creates a '_completed' folder" do
+      expect(File).to exist(path+"_completed")
+    end
+
+    it "creates a '_submitted' folder within the '_completed' folder" do
+      
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#move_to_submitted" do
+    context "in root folder" do
+      it "moves the current project folder to the completed/submitted folder" do
+        
+      end
+
+    end
+
+    context "already in completed folder" do
+      it "moves the current project folder into the completed/submitted folder" do
+        
+      end
+
+      it "doesn't create an additional completed folder inside the completed folder" do
+        
+      end
+
+    end
+
   end
+
 end
+
